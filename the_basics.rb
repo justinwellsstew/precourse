8115cadc494bcbefa89decfe7dc6a4a992d73568
Justin Stewart <stewartj@imac.cortland.edu>
2014-09-03 20:22:21 -0400

    first commit of precourse mat

---
+++ the_basics.rb
@@ -0,0 +1,40 @@
+#1 add two strings together
+"Justin" + "Stewart"
+
+#2 modulus and division
+1234/1000
+(1234 % 1000) /100
+((1234 % 1000) %100) /10
+((1234 % 1000) %100) %10
+
+
+#3 movies and years in hash
+movies = {Star_trek_1 : 1975, Star_trek_2 : 2004,  star_trek_3 : 2013, star_trek_4 : 2001, Star_wars : 1981}
+
+puts movies[:Star_trek_1]
+puts movies[:Star_trek_2]
+puts movies[:Star_trek_3]
+puts movies[:Star_trek_4]
+puts movies[:Star_trek_5]
+
+#4 movie years in an array
+years = [1975, 2004, 2013, 2001, 1981]
+
+puts years[0]
+puts years[1]
+puts years[2]
+puts years[3]
+puts years[4]
+
+#5 factorials
+puts 5*4*3*2*1
+puts 6*5*4*3*2*1
+puts 7*6*5*4*3*2*1
+puts 8*7*6*5*4*3*2*1
+
+#6 squares
+puts 3.0**2
+puts 10.1**2
+puts 11.2**4
+
+#7 there is an missing bracket
