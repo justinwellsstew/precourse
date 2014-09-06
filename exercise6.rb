639bac8bb6936c5e3360ff189a23ee9bf45890ab
Justin W. Stewart <stewartjustin3@gmail.com>
2014-09-06 18:35:14 -0400

    this is exercise 6 from the end of the precourse

---
+++ exercise6.rb
@@ -0,0 +1,8 @@
+#this program add a zero to the beginning of an array and 11 at the end, then takes the 11 off and adds a 3. 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers.unshift(0)
+numbers.push(11)
+numbers.pop
+numbers.push(3)
+numbers.uniq!
+puts numbers.to_s
