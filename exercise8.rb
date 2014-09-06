8e1d29439ee304d83e55f86922963225edf7b2be
Justin W. Stewart <stewartjustin3@gmail.com>
2014-09-06 18:41:38 -0400

    this is exercise 8 from the end of the precourse

---
+++ exercise8.rb
@@ -0,0 +1,7 @@
+# this file demostrates the two ways to create a hash
+
+cookies = {:vannila=> 8, :chocolate =>12, :strawberry =>42}
+puts cookies.to_s
+
+more_cookies ={oatmeal:9, chocolate_chip:6, moon_pie:44}
+puts more_cookies.to_s
